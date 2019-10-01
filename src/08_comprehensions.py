3f27990f00a014ccc696da3490a034a66a7003ed
Shola Ayeni <ayenisholah@yahoo.com>
2019-10-01 15:14:27 +0100

    list comprehension solution

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -17,18 +17,18 @@ for more info regarding list comprehensions.
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
-# y = [num**3 for num in range(10)]
+y = [num**3 for num in range(10)]
 
-# print(y)
+print(y)
 
 # Write a list comprehension to produce the uppercase version of all the
 # elements in array a. Hint: "foo".upper() is "FOO".
 
 a = ["foo", "bar", "baz"]
 
-# y = [element.upper() for element in a]
+y = [element.upper() for element in a]
 
-# print(y)
+print(y)
 
 # Use a list comprehension to create a list containing only the _even_ elements
 # the user entered into list x.
